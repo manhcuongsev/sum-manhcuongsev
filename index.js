e69f2fc974b2a561602d6fcc40bc3c0279c5dc59
root <root@vmi1633988.contaboserver.net>
2024-02-23 07:00:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    add : function(a,b) {
+        return a+b;
+    },
+    subtract : function(a,b){
+        return a-b;
+    },
+    multiply : function(a,b){
+        return a*b;
+    }
+     
+};
